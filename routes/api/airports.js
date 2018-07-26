5afa99473dec56096c6a04f39d3a64d24937ef41
jimyandres <jimyandres@utp.edu.co>
2018-07-26 05:25:58 -0500

    Get airports locations API endpoint added

--- routes/api/airports.js
+++ routes/api/airports.js
@@ -9,6 +9,12 @@ const router = express.Router();
 // config mongoose promise
 mongoose.Promise = global.Promise;
 
+// get from the DB all airports location
+const getAirportsLocation = async (res) => {
+  const locations = await Airport.find({}).select('name location _id');
+  return res.json({ locations: locations.map(l => l.toObject({ virtuals: true })) });
+};
+
 // Check if the airport exists and if not, save it
 const saveAirport = async (airportInfo, res) => {
   const airportQuery = await Airport.findOne({ code: airportInfo.code });
@@ -32,11 +38,24 @@ router.post('/', async (req, res) => {
 
   try {
     // Save it to the DB
-    return await saveAirport(airportInfo, res);
+    result = await saveAirport(airportInfo, res);
   } catch (err) {
     result = res.json({ error: 'There was an error saving the airport to the database. Please try again.' });
   }
 
+  return result;
+});
+
+// GET to /locations
+router.get('/locations', async (req, res) => {
+  let result;
+
+  try {
+    // Get all locations
+    return await getAirportsLocation(res);
+  } catch (e) {
+    result = res.json({ error: 'There was an error getting the airports location from the database. Please try again.' });
+  }
   return result;
 });
 
