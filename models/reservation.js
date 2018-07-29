381ccbcd5de7cfacab8203a2fea44f813766f231
jimyandres <jimyandres@utp.edu.co>
2018-07-29 08:41:27 -0500

    API endpoint to make reservations, added

--- models/reservation.js
+++ models/reservation.js
@@ -1,13 +1,28 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
+const User = require('./user');
 
 const Reservation = new Schema({
   flight: { type: Schema.Types.ObjectId, ref: 'Flight' },
   status: String,
-  date: Date,
   seatsReserved: Number,
   user: { type: Schema.Types.ObjectId, ref: 'User' },
+}, { timestamps: { createdAt: 'createdAt' } });
+
+Reservation.pre('save', function (next) {
+  User.findById(this.user, (err, userInfo) => {
+    if(err) {
+      next(err);
+    }
+    userInfo.reservations.push(this._id);
+    userInfo.save((e) => {
+      if (e) {
+        next(e);
+      }
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model('Reservation', Reservation);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 // Models
 const User = require('../../models/user');
+const Reservation = require('../../models/reservation');
 
 const router = express.Router();
 
@@ -18,6 +20,43 @@ const getUserInfo = async (userId, res) => {
     return res.json({ error: 'The user requested does not exists. Please check the information.' });
   }
   return res.json(userQuery);
+};
+
+// Check the restrictions to make the reservation
+const saveReservation = async (userId, reservationInfo, res) => {
+  // Get the user's info
+  await User.findOne({
+    id: userId,
+  })
+    .populate('reservations')
+    .exec(async (err, userInfo) => {
+      const { id, birthday, reservations } = userInfo;
+      if (id) {
+        // Add the user id to the reservation
+        Object.assign(reservationInfo, { user: userInfo._id });
+        // Check the age of the user
+        const age = moment().diff(birthday, 'years');
+        const lastReservation = reservations.filter(r => moment().diff(r.createdAt, 'hours') < 24);
+        if (age > 18) {
+          // Check the last reservation made by the user
+          if (lastReservation.length === 0) {
+            const newReservation = new Reservation(reservationInfo);
+            await newReservation.save((error, reservation) => {
+              if (error) {
+                return res.json({ error: 'There was an error saving the reservation to the database. Please try again.' });
+              }
+              return res.json(reservation);
+            });
+          } else {
+            return res.json({ error: "You can't make more than one reservation per day." });
+          }
+        } else {
+          return res.json({ error: 'You must have at least 18 years to make a reservation.' });
+        }
+      } else {
+        return res.json({ error: "The user doesn't exists. Please verify the information." });
+      }
+    });
 };
 
 // Check if the user exists and if not, save it
@@ -53,9 +92,9 @@ router.post('/', async (req, res) => {
   return result;
 });
 
-// Get to /:id
-router.get('/:id', async (req, res) => {
-  const userId = req.params.id;
+// GET to /:id
+router.get('/:user_id', async (req, res) => {
+  const userId = req.params.user_id;
   let result;
 
   try {
@@ -63,6 +102,22 @@ router.get('/:id', async (req, res) => {
     return await getUserInfo(userId, res);
   } catch (e) {
     result = res.json({ error: "There was an error getting the user's info. Please try again." });
+  }
+
+  return result;
+});
+
+// POST to /:user_id/reservations
+router.post('/:user_id/reservations', async (req, res) => {
+  const reservationInfo = req.body;
+  const userId = req.params.user_id;
+  let result;
+
+  try {
+    // Save the reservation to the DB
+    return await saveReservation(userId, reservationInfo, res);
+  } catch (e) {
+    result = res.json({ error: 'There was an error making the reservation. Please try again.' });
   }
 
   return result;
