4eab64c4d542708ed57be67aead174712a3d00a4
jimyandres <jimyandres@utp.edu.co>
2018-07-30 20:08:10 -0500

    Added getUserReservations helper

--- src/actions/reservations.js
+++ src/actions/reservations.js
@@ -6,6 +6,44 @@ import { clearError } from './error';
 const reservationFailure = error => ({ type: 'RESERVATION_FAILURE', error });
 const reservationFailureNoUser = json => ({ type: 'RESERVATION_FAILURE_NO_USER', json });
 const reservationSuccess = json => ({ type: 'RESERVATION_SUCCESS', json });
+const reservationListFailure = error => ({ type: 'RESERVATION_LIST_FAILURE', error });
+const reservationListSuccess = json => ({ type: 'RESERVATION_LIST_SUCCESS', json });
+
+// Get reservations
+const getUserReservations = (userId) => {
+  return async (dispatch) => {
+    // clear the error box if it's displayed
+    dispatch(clearError());
+
+    dispatch(incrementProgress());
+
+    // Send request to our API
+    await fetch(
+      // where to contact
+      `/api/users/${userId}/reservations`,
+      // what to send
+      {
+        method: 'GET',
+        credentials: 'same-origin',
+      },
+    ).then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      return null;
+    }).then((json) => {
+      if (json.reservations) {
+        return dispatch(reservationListSuccess(json));
+      }
+      return dispatch(reservationListFailure(new Error(json.error)));
+    }).catch((error) => {
+      return dispatch(reservationListFailure(new Error(error)));
+    });
+
+    // turn off spinner
+    return dispatch(decrementProgress());
+  };
+};
 
 // Make a reservation
 const requestReservation = (userId, params) => {
@@ -58,5 +96,6 @@ export {
   reservationFailureNoUser,
   reservationSuccess,
   // Helpers
+  getUserReservations,
   requestReservation,
 };
