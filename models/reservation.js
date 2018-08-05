31054d6fc78519c46e79dcdff1ea0bd073d9750e
jimyandres <jimyandres@utp.edu.co>
2018-08-05 14:53:29 -0500

    Backend validation (reservations) and some minor changes (eslint)

--- models/reservation.js
+++ models/reservation.js
@@ -4,15 +4,31 @@ const { Schema } = mongoose;
 const User = require('./user');
 
 const Reservation = new Schema({
-  flight: { type: Schema.Types.ObjectId, ref: 'Flight' },
-  status: String,
-  seatsReserved: Number,
-  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  flight: {
+    type: Schema.Types.ObjectId,
+    ref: 'Flight',
+    required: [true, 'No flight given'],
+  },
+  status: {
+    type: String,
+    required: [true, 'No status given'],
+  },
+  seatsReserved: {
+    type: Number,
+    min: 1,
+    max: 8,
+    required: [true, 'No seats reserved given'],
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'No User given'],
+  },
 }, { timestamps: { createdAt: 'createdAt' } });
 
 Reservation.pre('save', function (next) {
   User.findById(this.user, (err, userInfo) => {
-    if(err) {
+    if (err) {
       next(err);
     }
     userInfo.reservations.push(this._id);
